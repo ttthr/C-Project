03d4105832787e06280dd965fa6b6c21c84b0097
tcd159 <ttthr@naver.com>
2019-06-19 15:19:34 +0900

    ㄱ

--- RandomNumber/main.cpp
+++ RandomNumber/main.cpp
@@ -3,7 +3,33 @@
 
 using namespace std;
 
+// 문제
+// 숫자 X보다 크거나 같은 수 중 가장 적은 2의 승수(2^n)를 NPOT(Next Power of Two)라고 할 때,
+// 주어진 숫자 N개의 NPOT을 구하여 모두 XOR한 값을 구하시오
+
+// 조건
+// 1 < X < 2^63
+// 1 <= N < 2^18
+
+// 예시
+// 입력 
+// 3 : 숫자의갯수
+// 3
+// 5
+// 7
+
+// 출력 
+// 4
+// 4 = 4 ^ 8 ^ 8 ( 4 xor 8 xor 8 )
+// xor : 둘중에 하나가 1이면 1 / 둘다 같으면 0 
+
+// 9,223,372,036,854,775,807  ( (2^63) - 1 의 값 )
+// 입출력 리다이렉션과 파이프
+
 typedef unsigned long long uint64;
+//범위가 정해지면
+//type 지정하는것을 물어보는 것
+// 9223372036854775807 
 
 int main()
 {
@@ -12,9 +38,11 @@ int main()
 	vector<uint64> NPOTStorage;
 	int N = 0;
 	uint64 X = 0;
+	cout << "숫자 갯수 입력: ";
 	cin >> N;
 
 	//입력
+	cout << "값 입력:";
 	for (int i = 0; i < N; ++i)
 	{
 		cin >> X;
